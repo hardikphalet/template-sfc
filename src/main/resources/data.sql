14c9567004e1a7998f77fda3d91749a995b860f6
Hardik Phalet <hardik.phalet@captainfresh.in>
2022-09-19 15:37:54 +0530

    major

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO regions (id, name, parent_id, population, created_by, updated_by) VALUES      (1, 'USA',,123213123, 'System', 'System');
-INSERT INTO regions (id, name, parent_id, population, created_by, updated_by) VALUES   (2, 'France',,123213123, 'System', 'System');
-INSERT INTO regions (id, name, parent_id, population, created_by, updated_by) VALUES   (3, 'Brazil',,123213123, 'System', 'System');
-INSERT INTO regions (id, name, parent_id, population, created_by, updated_by) VALUES    (4, 'Italy',,123213123, 'System', 'System');
-INSERT INTO regions (id, name, parent_id, population, created_by, updated_by) VALUES   (5, 'Canada',,123213123, 'System', 'System');
-INSERT INTO regions (id, name, parent_id, population, created_by, updated_by) VALUES (6, 'Toronto',5,123213123, 'System', 'System');
